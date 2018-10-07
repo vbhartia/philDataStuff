0c6043ff853101ebad7fc431883c4b666f83ecb7
Varun Bhartia <vbhartia@mba2014.hbs.edu>
2018-10-07 01:21:21 -0700

    added stuff

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,16 +4,170 @@ import Wrap from '../components/Wrap';
 import data from '../data/main';
 import AOS from 'aos';
 
-import ReactHighcharts from 'react-highcharts'; // Expects that Highcharts was loaded in the code.
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
 
-const config = {
+// for Turn out in Nigeria
+const config2 = {
+    chart: {
+        type: 'area',
+        style: {
+          fontFamily: 'Arial, sans-serif',
+        },
+        backgroundColor: 'transparent',
+    },
+    style: {
+      fontFamily: 'Arial, sans-serif',
+    },
+    backgroundColor: 'transparent',
+    title: {
+        text: 'Turnout in Nigerian Elections'
+    },
+    subtitle: {
+        text: '2007 to 2015'
+    },
     xAxis: {
-      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        categories: ['1999', '2003', '2007', '2011', '2015']
+    },
+    yAxis: {
+        title: {
+            enabled: false,
+            //text: 'Election Turnout'
+        },
+        min: 40,
+        labels: {
+            formatter: function () {
+                return this.value+"%";
+            }
+        }
+    },
+    tooltip: {
+        formatter: function() {
+          return 'In <b>' + this.x + '</b> turnout was <b>' + this.y + '</b>% ';
+        }
+
+    },
+    plotOptions: {
+        area: {
+            tickInterval: 4,
+            marker: {
+                enabled: false,
+                symbol: 'circle',
+                stroke: 5,
+                radius: 5,
+                states: {
+                    hover: {
+                        enabled: true
+                    }
+                }
+            }
+        }
     },
     series: [{
-      data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 295.6, 454.4]
+        name: 'Turnout',
+        color: '#0e677f',
+        data: [52, 69, 57, 54, 44]
+    }],
+    legend: {
+        enabled: false
+    },
+    credits: {
+        enabled: false
+    }
+}
+
+
+// Data gathered from http://populationpyramid.net/germany/2015/
+// Age categories
+var categories = [
+    'Ebonyi', 'Edo', 'Ekiti', 'FCT',
+    'Kebbi', 'Kwara', 'Ondo', 'Taraba', 'Oyo',
+    'Jigawa', 'Ogun', 'Bayelsa', 'Lagos', 'Adamawa',
+    'Kogi', 'Plateau', 'Zamfara', 'Niger', 'Delta',
+    'Gombe', 'Bauchi', 'Imo', 'Osun', 'Yobe', 'Borno', 'Enugu',
+    'Nassarawa', 'Rivers', 'Benue', 'Akwa Ibom', 'Cross River',
+    'Katsina', 'Kano', 'Sokoto', 'Abia', 'Kaduna', 'Anambra'
+];
+
+var config3 = {
+    chart: {
+        type: 'bar',
+        style: {
+          fontFamily: 'Arial, sans-serif',
+        },
+        height: 580,
+        backgroundColor: 'transparent',
+    },
+    title: {
+        text: 'Percent of Polling Units in Which Voters Were Accredited to Vote without the PVC Reader Checking Their Card',
+    },
+    subtitle: {
+        text: '(Click on the legend to explore by year.)'
+    },
+    xAxis: [{
+        categories: categories,
+        reversed: false,
+        labels: {
+            step: 1
+        }
+    }, { // mirror axis on right side
+        opposite: true,
+        reversed: false,
+        categories: categories,
+        linkedTo: 0,
+        labels: {
+            step: 1
+        }
+    }],
+    yAxis: {
+        title: {
+            text: null
+        },
+        labels: {
+            formatter: function () {
+                return Math.abs(this.value) + '%';
+            }
+        }
+    },
+
+    plotOptions: {
+        series: {
+            stacking: 'normal'
+        }
+    },
+
+    tooltip: {
+        formatter: function () {
+            return this.series.name + ' in ' + this.point.category + ' state:' + '<br/>' +
+                '<b>' + Highcharts.numberFormat(Math.abs(this.point.y), 0) + '%' + '</b>';
+        }
+    },
+    credits: {
+        enabled: false
+    },
+    series: [{
+        name: 'Percent of PUs in 2011',
+        color: '#0e677f',
+        data: [
+            0, 0, 0, 0, 0, 0, 0, 0, -3, -4,
+            -7, -9, -10, -11, -11, -11,
+            -12, -12, -13, -13, -14, -16,
+            -16, -17, -19, -19, -19, -19,
+            -32, -35, -35, -36, -38, -38,
+            -43, -43, -52
+        ]
+    }, {
+        name: 'Percent of PUs in 2015',
+        color: '#81d2ff',
+        data: [
+            26, 56, 36, 0, 6, 8, 21, 42, 56, 27,
+            44, 20, 41, 52, 51, 56, 26, 51, 52, 26,
+            48, 64, 27, 42, 53, 46, 58, 54, 56, 52,
+            55, 59, 53, 62, 70, 52, 56
+        ]
     }]
-  };
+};
+
 
 export class Main extends Component {
     constructor(props) {
@@ -54,7 +208,15 @@ export class Main extends Component {
                         : ('Data not found')
                     };
                 </div>
-                <ReactHighcharts config={config}/>
+                <HighchartsReact
+                    highcharts={Highcharts}
+                    options={config2}
+                />
+                <HighchartsReact
+                    highcharts={Highcharts}
+                    options={config3}
+                />
+                
             </Wrap>
         );
     }
